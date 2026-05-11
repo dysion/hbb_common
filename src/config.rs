82aab0dee59fbef9c1afb1c084e762b6387b8db8
dysion <support@dysion.cn>
2026-05-11 19:15:55 +0800

    update cfg 3

--- src/config.rs
+++ src/config.rs
@@ -156,7 +156,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["test2.com"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["test2-3.com"];
 pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
